8e172800d726937a389037e5d7c4b67d954d0de3
LucyFerrr <akieranscott99@gmail.com>
2025-04-19 10:18:18 +0800

    Initial Commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,46 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  // Get initial users (Testing Purposes)
+  // const fetchUsers = async () => {
+  //   setLoading();
+  //   const response = await fetch(`${GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   });
+
+  //   const data = await response.json();
+  //   console.log(data);
+
+  //   dispatch({
+  //     type: "GET_USERS",
+  //     payload: data,
+  //   });
+  // };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
